c71cfce9c8122a9f211ca20563ff14b729bf443a
liucannn@sina.com <liucannn@sina.com>
2021-01-04 22:18:00 +0800

    feat: tasks

--- src/renderer/pages/tasks/index.tsx
+++ src/renderer/pages/tasks/index.tsx
@@ -1,7 +1,30 @@
 import * as React from 'react';
 
 const Tasks: React.FC = () => {
-  return <div className="tasks">Tasks</div>;
+  return (
+    <div className="tasks">
+      <header>Header</header>
+      <main>
+        <input type="text" placeholder="新增任务" />
+        <div>
+          未完成
+          <ul>
+            <li>1</li>
+            <li>1</li>
+            <li>1</li>
+          </ul>
+        </div>
+        <div>
+          已完成
+          <ul>
+            <li>1</li>
+            <li>1</li>
+            <li>1</li>
+          </ul>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Tasks;
